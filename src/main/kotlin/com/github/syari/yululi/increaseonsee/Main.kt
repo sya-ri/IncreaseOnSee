abe74e82938eab0a990393a3fe0ec09a648896cf
sya-ri <34268371+sya-ri@users.noreply.github.com>
2021-04-12 16:44:47 +0900

    Initial commit

---
+++ src/main/kotlin/com/github/syari/yululi/increaseonsee/Main.kt
@@ -0,0 +1,13 @@
+package com.github.syari.yululi.increaseonsee
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    companion object {
+        lateinit var plugin: JavaPlugin
+    }
+
+    init {
+        plugin = this
+    }
+}
